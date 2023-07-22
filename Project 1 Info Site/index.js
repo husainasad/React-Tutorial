ae1320de04dd94eb9eb6ae953b79bff7ad6025ea
husainasad <asad1999@gmail.com>
2023-07-22 15:42:15 -0700

    used components for reusability

--- Project 1 Info Site/index.js
+++ Project 1 Info Site/index.js
@@ -15,10 +15,49 @@
 // )
 
 // functional way
+// function InfoComponent(){
+//     return (
+//         <div>
+//             <header>
+//                 <nav>
+//                     <img src="react-logo.svg" alt="react logo"width="80"></img>
+//                 </nav>
+//             </header>
+//             <h1>Why React?</h1>
+//             <ul>
+//                 <li>Reusable Components</li>
+//                 <li>Responsive</li>
+//                 <li>Maintained by Meta</li>
+//                 <li>Big Community</li>
+//                 <li>Framework of choice by majority of developers</li>
+//             </ul>
+//             <footer>
+//                 <small>© 2023 Husain Development. All rights reserved</small>
+//             </footer>
+//         </div>
+//     )
+// }
+
+// ReactDOM.render(<InfoComponent/>, document.getElementById("root"))
+
+function HeaderComponent(){
+    return (
+        <header>
+            <nav className="nav-bar">
+                <img className= "nav-logo" src="react-logo.svg" alt="react logo"></img>
+                <ul className="nav-items">
+                    <li>Pricing</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
 function InfoComponent(){
     return (
         <div>
-            <img src="react-logo.svg" alt="react logo"width="80"></img>
             <h1>Why React?</h1>
             <ul>
                 <li>Reusable Components</li>
@@ -31,4 +70,24 @@ function InfoComponent(){
     )
 }
 
-ReactDOM.render(<InfoComponent/>, document.getElementById("root"))
+function FooterComponent(){
+    return (
+        <footer>
+            <div className="foot-bar">
+                © 2023 Husain Development. All rights reserved
+            </div>
+        </footer>
+    )
+}
+
+function PageContent(){
+    return (
+        <>
+            <HeaderComponent/>
+            <InfoComponent/>
+            <FooterComponent/>
+        </>
+    )
+}
+
+ReactDOM.render(<PageContent/>, document.getElementById("root"))
