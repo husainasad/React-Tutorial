1ca2803db7a81cf6e871e1179691bab01ee6e81b
husainasad <asad1999@gmail.com>
2023-08-01 02:22:21 -0700

    added navigation styling

--- info-site/src/components/Navbar.js
+++ info-site/src/components/Navbar.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import '../index.css'
+import logo from '../logo.svg';
 
 export default function Navbar(){
     return(
-        <h1>This is Navbar</h1>
+        <div className="Nav">
+            <div className="NavLeft">
+                <img src={logo} alt="React-logo"></img>
+                <span>ReactFacts</span>
+            </div>
+            <div className="NavRight">
+                React Project Info Site
+            </div>
+        </div>
     )
 }
